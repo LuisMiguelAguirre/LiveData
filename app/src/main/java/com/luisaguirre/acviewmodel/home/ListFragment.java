0fd10996d3d82def285420ef83cf520ab583e154
Luis Aguirre <kourosvi@gmail.com>
2019-03-09 19:11:04 -0500

    Initial commit

---
+++ app/src/main/java/com/luisaguirre/acviewmodel/home/ListFragment.java
@@ -0,0 +1,6 @@
+package com.luisaguirre.acviewmodel.home;
+
+import android.support.v4.app.Fragment;
+
+public class ListFragment extends Fragment {
+}
---
+++ app/src/main/java/com/luisaguirre/acviewmodel/home/MainActivity.java
@@ -0,0 +1,15 @@
+package com.luisaguirre.acviewmodel.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.luisaguirre.acviewmodel.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
